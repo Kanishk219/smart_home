49c8d18c22c05fbe32eb376ff3e09947d55dccdb
kanishk <134833626+Kanishk219@users.noreply.github.com>
2025-03-04 22:56:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import EnergyUsage from "./components/Energyusage";
+import './App.css';
+function App(){
+return (
+  <div className="App">
+    <h1>Smart home energy monitoring</h1>
+    <EnergyUsage />
+  </div>
+)
+}
